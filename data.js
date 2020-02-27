a20e32a3f2eff4eef9b64a25189728e023b2ed22
NicolaVermeersch <61147861+NicolaVermeersch@users.noreply.github.com>
2020-02-27 10:34:55 +0100

    Update data.js

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/a6e937ce-81ce-4212-58ac-40f8fae4d8be-d0d8/",
                 "target": "_blank"
             }
         ]
